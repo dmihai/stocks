bb61dc35b214c788c77dd5a273357d268b301e14
Mihai Doarna <dmihai@gmail.com>
2024-03-30 11:59:40 +0200

    add support for mysql

--- utils/Database.py
+++ utils/Database.py
@@ -11,25 +11,75 @@ class Database(ABC):
     def connect(self, config):
         pass
 
-    @abstractmethod
     def get_next_unpopulated_symbol(self):
-        pass
+        cursor = self._conn.cursor()
 
-    @abstractmethod
+        query = "SELECT symbol, history_start, history_end\
+            FROM stocks\
+            WHERE history IS NULL AND symbol NOT LIKE '%-%' AND last_update IS NULL\
+            LIMIT 1"
+        cursor.execute(query)
+
+        result = cursor.fetchone()
+
+        if result is None:
+            return None
+
+        return {
+            "symbol": result[0],
+            "start": result[1],
+            "end": result[2]
+        }
+
     def get_next_symbols_for_update(self, limit: int):
-        pass
+        cursor = self._conn.cursor()
 
-    @abstractmethod
+        query = "SELECT symbol, history_end\
+            FROM stocks\
+            WHERE status='Active' AND history_start IS NOT NULL AND last_update<?\
+            ORDER BY history_end\
+            LIMIT ?"
+        cursor.execute(query, (self.today(), limit))
+
+        return cursor.fetchall()
+
     def insert_history(self, history):
-        pass
+        cursor = self._conn.cursor()
 
-    @abstractmethod
+        cursor.executemany(
+            "REPLACE INTO history (symbol, date, open, high, low, close, volume, dividends, splits) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", history)
+        self._conn.commit()
+
     def insert_stocks(self, stocks):
-        pass
+        cursor = self._conn.cursor()
 
-    @abstractmethod
+        cursor.executemany("""INSERT OR IGNORE INTO stocks
+            (symbol, name, exchange, asset_type, ipo_date, delisting_date, status)
+            VALUES (?, ?, ?, ?, ?, ?, ?)""", stocks)
+        self._conn.commit()
+
+        return cursor.rowcount
+
     def update_symbol_history(self, symbol, history, history_start, history_end):
-        pass
+        cursor = self._conn.cursor()
+
+        update_fields = "history = ?, last_update = ?"
+        params = (history, self.today())
+        
+        if history_start is not None:
+            update_fields += ", history_start = ?"
+            params += (history_start,)
+        
+        if history_end is not None:
+            update_fields += ", history_end = ?"
+            params += (history_end,)
+        
+        query = f"UPDATE stocks\
+            SET {update_fields}\
+            WHERE symbol = ?"
+        cursor.execute(query, params + (symbol,))
+        
+        self._conn.commit()
 
     def today(self):
         return str(date.today())
--- utils/DatabaseMysql.py
+++ utils/DatabaseMysql.py
@@ -5,22 +5,12 @@ from utils.Database import Database
 
 class DatabaseMysql(Database):
     def __del__(self):
-        pass
+        self._conn.close()
 
     def connect(self, config):
-        pass
-
-    def get_next_unpopulated_symbol(self):
-        pass
-
-    def get_next_symbols_for_update(self, limit: int):
-        pass
-
-    def insert_history(self, history):
-        pass
-
-    def insert_stocks(self, stocks):
-        pass
-
-    def update_symbol_history(self, symbol, history, history_start, history_end):
-        pass
+        self._conn = mysql.connector.connect(
+            host=config['host'],
+            user=config['user'],
+            password=config['password'],
+            database=config['database']
+        )
