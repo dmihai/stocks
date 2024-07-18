6df7d52962267357a9c6e9942c702ed4f3580639
Mihai Doarna <dmihai@gmail.com>
2024-07-18 22:29:18 +0300

    add query to populate history_month

--- utils/DatabaseMysql.py
+++ utils/DatabaseMysql.py
@@ -17,3 +17,30 @@ class DatabaseMysql(Database):
     
     def _transform_query(self, query):
         return query
+
+    def populate_history_month(self):
+        cursor = self._conn.cursor()
+
+        query = "INSERT INTO history_month\
+                (symbol, date, open, high, low, close, volume)\
+            SELECT\
+                symbol,\
+                DATE_SUB(date, INTERVAL (DAY(date)-1) DAY) month,\
+                (SELECT open\
+                    FROM history\
+                    WHERE symbol=h.symbol AND YEAR(date)=YEAR(month) AND MONTH(date)=MONTH(month)\
+                    ORDER BY date ASC\
+                    LIMIT 1),\
+                MAX(high),\
+                MIN(low),\
+                (SELECT close\
+                    FROM history\
+                    WHERE symbol=h.symbol AND YEAR(date)=YEAR(month) AND MONTH(date)=MONTH(month)\
+                    ORDER BY date DESC\
+                    LIMIT 1),\
+                SUM(volume)\
+            FROM history h\
+            GROUP BY symbol, DATE_SUB(date, INTERVAL (day(date)-1) DAY)"
+        cursor.execute(query)
+
+        self._conn.commit()
