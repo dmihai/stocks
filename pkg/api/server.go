f72ea7dfdb52935a140b8353583f4a6926945633
Mihai Doarna <dmihai@gmail.com>
2024-09-08 17:11:27 +0300

    init api server

---
+++ pkg/api/server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	addr string
+}
+
+func NewServer(addr string) *Server {
+	return &Server{
+		addr: addr,
+	}
+}
+
+func (s *Server) Start() {
+	r := setupRouter()
+
+	r.Run(s.addr)
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	return r
+}
